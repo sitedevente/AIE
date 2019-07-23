7637ab1643287f81e2b9ae56527c7d235d3b1c4a
TipocheJM <martinjarod17@gmail.com>
2019-07-23 19:41:06 +0200

    Update BienDb.js
    Added environment variable stored in process.env.

--- models/BienDb.js
+++ models/BienDb.js
@@ -3,12 +3,12 @@ const mysql2 = require('mysql2/promise');
 module.exports = class BienDb{
 	constructor (){
 		return mysql2.createPool({
-			host: 'localhost',
-			user : 'aie' ,
-			password: '21504680',
-			database: 'aie',
-			connectionLimit : 10,
-			queueLimit : 150
+			host: process.env.DbBien_HOST || 'localhost',
+			user : process.env.DbBien_USER || 'aie' ,
+			password: process.env.DbBien_PASSWORD || '21504680',
+			database: process.env.DbBien_DATABASE || 'aie',
+			connectionLimit : process.env.DbBien_CONNECTIONLIMIT || 10,
+			queueLimit : process.env.DbBien_QUEUELIMIT || 150
 		});
 	}
 };
