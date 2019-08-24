18986efc4abb8b02802886bdbf7be73f0b43b9c6
TipocheJM <martinjarod17@gmail.com>
2019-08-25 01:45:33 +0200

    New database creation script
    In this project, Sequelize-Cli module not being used,
    a script was created to replace it.

---
+++ createDatabase.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const { database, ModelCreator } = require('./models');
+
+database.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    })
+
+
+const { EstateCreator, FlatCreator, HouseCreator } = ModelCreator;
+
+const Estate = EstateCreator(database, Sequelize);
+
+const Flat = FlatCreator(database, Sequelize, Estate);
+
+const House = HouseCreator(database, Sequelize, Estate);
+
+database.sync()
+    .then(() => {
+        console.log('Sucessfully created')
+    })
+    .catch(err => {
+        console.error(err)
+    });
