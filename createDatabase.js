c331cd268d95753e8fc1afd9fd4628e0e221bbd3
TipocheJM <martinjarod17@gmail.com>
2019-08-25 05:25:33 +0200

    Update createDatabase.js

--- createDatabase.js
+++ createDatabase.js
@@ -2,12 +2,12 @@ const Sequelize = require('sequelize');
 const { database, ModelCreator } = require('./models');
 
 database.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    })
+.then(() => {
+    console.log('Connection has been established successfully.');
+})
+.catch(err => {
+    console.error('Unable to connect to the database:', err);
+})
 
 
 const { EstateCreator, FlatCreator, HouseCreator } = ModelCreator;
@@ -18,10 +18,11 @@ const Flat = FlatCreator(database, Sequelize, Estate);
 
 const House = HouseCreator(database, Sequelize, Estate);
 
-database.sync()
-    .then(() => {
-        console.log('Sucessfully created')
-    })
-    .catch(err => {
-        console.error(err)
-    });
+database.sync({
+    forces: true
+}).then(() => {
+    console.log('Sucessfully created')
+})
+.catch(err => {
+    console.error(err)
+});
