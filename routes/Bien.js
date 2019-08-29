e22cf7f7563ca0ca83cfdf7e49e047d214d9654c
TipocheJM <martinjarod17@gmail.com>
2019-08-29 17:16:58 +0200

    Custom joi middleware
    Allow early failure if json object is not valid.
    Does not trigger controller to save data anymore.
    And seperate validation from the database access layer.

--- routes/Bien.js
+++ routes/Bien.js
@@ -1,16 +1,35 @@
-const {Router,json} = require('express');
+const {Router, json} = require('express');
 const BienController = require('../controller/BienController');
+const EstateJoi = require('../models/joi/BienJoi');
+
 
 const bienCtrl = new BienController();
 const router = new Router();
 
-// problem here in body req
+const estateValidator = async (req, res, next) => {
+    const {flat, house} = req.body;
+    let type = '';
+    if(flat){
+        type = 'flat';
+    }else if (house){
+        type = 'house';
+    }
+    const schema = new EstateJoi(type);
+
+    await schema.validate(req.body)
+    .then(() => {
+        req.body.type = type;
+        next();
+    })
+    .catch(err => res.status(400).json(err.details))
+};
+
 
 router
-.param('id',BienController.idIsValid)
-.post('/', json() , bienCtrl.setBien.bind(bienCtrl)) 
+.param('id', BienController.idIsValid)
+.post('/', json(), estateValidator, bienCtrl.setBien.bind(bienCtrl))
 .get('/', bienCtrl.getBien.bind(bienCtrl))
 .get('/:id', bienCtrl.getBien.bind(bienCtrl))
-.use('*',BienController.notFound)
+.use('*', BienController.notFound)
 
 module.exports = router;
